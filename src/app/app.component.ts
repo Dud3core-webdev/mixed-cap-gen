39f35f22c61e541067ee4b28afd41addee442ed8
Dudecor3 <freeman.marc880@gmail.com>
2020-04-06 12:47:04 +0100

    First commit, head to https://mixedcapgen.web.app/ to see

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mixed-cap-gen';
+export class AppComponent implements OnInit {
+  public mixedCapFormControl: FormControl;
+  public mixedCaps: string;
+
+  constructor() {
+    this.mixedCapFormControl = new FormControl('', Validators.required);
+  }
+
+  public ngOnInit(): void {
+    this.mixedCapFormControl.valueChanges
+      .subscribe((value) => this.mixedCaps = this.convertToMixedCaps(value));
+  }
+
+  public convertToMixedCaps(value: string): string {
+    const allUpperCase = value.toUpperCase();
+    let mixedCaps = '';
+
+    for (let i = 0; i < allUpperCase.length; i++) {
+      if (i % 2 === 0) {
+        mixedCaps = mixedCaps.concat(allUpperCase[i]);
+      } else {
+        mixedCaps = mixedCaps.concat(allUpperCase[i].toLowerCase())
+      }
+    }
+
+    return mixedCaps;
+  }
+
 }
